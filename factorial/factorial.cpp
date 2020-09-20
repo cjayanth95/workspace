9d629098c7e96d24a4d13b47777d56f7065de03c
Jayanth Culli <jculli@adobe.com>
2020-09-20 17:45:48 +0530

    Add factorial and build tasks

---
+++ factorial/factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int factorial(int n) {
+    cout<<"hit here"<<endl;
+  int result = 1;
+  while (n) {
+      cout<<"hit here as well"<<endl;
+    result *= n;
+    n--;
+    cout<<result<<endl;
+  }
+  return result;
+}
+int main() {
+  int n = 10;
+  int result = factorial(n);
+  cout << result << endl;
+  return result;
+}
