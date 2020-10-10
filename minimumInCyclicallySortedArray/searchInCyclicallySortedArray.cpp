3fdd0060b86db7997e86890483822472778a9ca3
Jayanth Culli <jculli@adobe.com>
2020-10-10 14:24:18 +0530

    repitions in cyclically sorted array

--- minimumInCyclicallySortedArray/searchInCyclicallySortedArray.cpp
+++ minimumInCyclicallySortedArray/searchInCyclicallySortedArray.cpp
@@ -17,11 +17,27 @@ int minimumInCyclicallySorted(vector<int> &A) {
   }
   return L;
 }
+// in case of repititions
+//
+int SearchSmallestHelper(const vector<int> &A, int left, int right) {
+  if (left == right)
+    return left;
+  int middle = left + (right - left) / 2;
+  if (A[middle] > A[right])
+    return SearchSmallestHelper(A, middle + 1, right);
+  else if (A[middle] < A[right])
+    return SearchSmallestHelper(A, left, middle);
+  else {
+    int leftSmallest = SearchSmallestHelper(A, left, middle);
+    int rightSmallest = SearchSmallestHelper(A, middle + 1, right);
+    return A[leftSmallest] > A[rightSmallest] ? rightSmallest : leftSmallest;
+  }
+}
 int main() {
 
   // decreasing then increasing
   vector<int> A = {378, 478, 550, 631, 103, 203, 220, 234, 279, 368};
   cout << &A << endl;
   int min = minimumInCyclicallySorted(A);
-  cout<<min<<endl;
+  cout << min << endl;
 }
