d66764aa775d2a30320bd3f1663c4d0e4e529e23
Jayanth Culli <jculli@adobe.com>
2020-10-17 12:15:13 +0530

    adding to stack with maxapi

--- stackWithMaxAPI/stackWithAPI.cpp
+++ stackWithMaxAPI/stackWithAPI.cpp
@@ -2,34 +2,55 @@
 #include <math.h>
 #include <new>
 #include <stack>
+#include <stdexcept>
 
 using namespace std;
 
 class Stack {
 private:
-  struct MaxElementWithCache {
-    int element;
+  struct MaxElementWithCount {
     int max;
+    int count;
   };
-  stack<MaxElementWithCache> element_with_cached_max;
+  stack<MaxElementWithCount> element_with_cached_max;
+  stack<int> element_;
 
 public:
-  bool Empty() const { return element_with_cached_max.empty(); }
+  bool Empty() const { return element_.empty(); }
   int Max() const {
     if (Empty())
-      throw __throw_length_error;
+      throw length_error("stack empty");
     int pop_element = element_with_cached_max.top().max;
     return pop_element;
   }
   void Push(int x) {
+    element_.emplace(x);
+    if (element_with_cached_max.empty()) {
+      element_with_cached_max.emplace(MaxElementWithCount{x, 1});
+    } else {
+      int max = element_with_cached_max.top().max;
+      if (max == x) {
+        int &maxCount = element_with_cached_max.top().max;
+        ++maxCount;
+      } else if (x > max) {
+        element_with_cached_max.emplace(MaxElementWithCount{x, 1});
+      }
+    }
     element_with_cached_max.emplace(
-        MaxElementWithCache{x, max(x, Empty() ? x : Max())});
+        MaxElementWithCount{x, max(x, Empty() ? x : Max())});
   }
   int Pop() {
     if (Empty())
-      throw __throw_length_error;
-    int pop_element = element_with_cached_max.top().element;
-    element_with_cached_max.pop();
+      throw length_error("stack empty");
+    int pop_element = element_.top();
+    element_.pop();
+    int max = element_with_cached_max.top().max;
+    if (pop_element == max) {
+      int &maxCount = element_with_cached_max.top().max;
+      --maxCount;
+      if (maxCount == 0)
+        element_with_cached_max.pop();
+    }
     return pop_element;
   }
 };
